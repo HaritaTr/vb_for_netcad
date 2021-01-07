e09a5ecd9314e6874dc8880b02ce8d7030bfa0d2
Erdem <57351007+ErdemGns@users.noreply.github.com>
2021-01-07 21:57:42 +0300

    Create TopAlanHesab.vb

---
+++ TopAlanHesab.vb
@@ -0,0 +1,19 @@
+' Açıklama : Seçilen alan objelerinin toplam alanını vermeyi sağlayan makro.
+
+Sub Main
+Dim i,sayi,secim,obje,alan
+  with Netcad
+    Set secim=.NewSelectionset
+    secim.SELECT "Lütfen alan toplamlarını hesaplamak istediğiniz alan objelerini seçiniz.", Array(opline)
+    If .EscPressed then Exit Sub
+    sayi=secim.NE-1
+    alan=0
+    Set obje=.NewObject
+    For i=0 to sayi
+        secim.GetSelectedObject i, obje
+        alan=alan + obje.Area
+    Next
+    .Message 0, "Toplam Alan: " & Round((alan),4) & " m² ", "Alan hesaplandı!"
+  end with
+End Sub
+
