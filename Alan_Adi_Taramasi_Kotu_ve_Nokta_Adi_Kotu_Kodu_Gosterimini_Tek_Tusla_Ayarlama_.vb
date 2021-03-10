5d8bd1032da23c35dda5351e0f9cbf4684727952
Erdem <57351007+ErdemGns@users.noreply.github.com>
2021-03-10 15:41:04 +0300

    Update Alan_Adi_Taramasi_Kotu_ve_Nokta_Adi_Kotu_Kodu_Gosterimini_Tek_Tusla_Ayarlama_.vb

--- Alan_Adi_Taramasi_Kotu_ve_Nokta_Adi_Kotu_Kodu_Gosterimini_Tek_Tusla_Ayarlama_.vb
+++ Alan_Adi_Taramasi_Kotu_ve_Nokta_Adi_Kotu_Kodu_Gosterimini_Tek_Tusla_Ayarlama_.vb
@@ -1,11 +1,6 @@
 Sub Main
 
 with netcad
-'' www.sabangul.com.tr Web Sayfasından İndirilmiştir
-' Şaban GÜL , Harita Mühendisi
-' Her Türlü Hata, İstek ve Öneriler İçin 
-' haritaakademi@gmail.com veya sagulnet@gmail.com
-' adresine durumu anlatan bir e-posta gönderiniz.
       .setparam 25,1     ' nokta ad
       .setparam 26,0     ' nokta kot
       .setparam 27,0     ' nokta kod
